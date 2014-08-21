cbff9ef77d46b39789231095cacd99bfa647f2d8
Richard Hamilton <richardgregoryhamilton@gmail.com>
2014-08-21 16:57:09 -0500

    changed database

--- db/migrate/20140801223107_add_ratings_to_games.rb
+++ db/migrate/20140801223107_add_ratings_to_games.rb
@@ -1,7 +1,6 @@
 class AddRatingsToGames < ActiveRecord::Migration
   def change
-    add_column :games, :ratings_count, :integer, null: false, default: 0
-    add_column :games, :rating_total, :integer, null: false, default: 0
-	add_column :games, :rating, :decimal, null: false, default: 0
+    add_column :games, :ratings_count, :integer
+    add_column :games, :rating_total, :integer
   end
 end
